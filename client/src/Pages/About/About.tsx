ea9193d12ff9df235184b565dc4f1eaeb74f7db5
Nich Secord <nicholassecord@gmail.com>
2019-09-27 15:49:27 -0700

    added link to nyt api

--- client/src/Pages/About/About.tsx
+++ client/src/Pages/About/About.tsx
@@ -13,42 +13,46 @@ const About: React.FC<Props> = () => {
                 <p>
                     Mongo Headlines is built using the MERN stack of technologies. It uses a{' '}
                     <a href='https://nodejs.org/en/' target='blank'>
-                        NodeJS <i className='far fa-external-link'></i>
+                        NodeJS <i className='far fa-external-link' />
                     </a>{' '}
                     powered backend, and the{' '}
                     <a href='https://www.npmjs.com/package/express' target='blank'>
-                        Express <i className='far fa-external-link'></i>
+                        Express <i className='far fa-external-link' />
                     </a>{' '}
                     framework for its server. The database is{' '}
                     <a href='https://www.mongodb.com/' target='blank'>
-                        MongoDB <i className='far fa-external-link'></i>
+                        MongoDB <i className='far fa-external-link' />
                     </a>
                     , connected to the server with the{' '}
                     <a href='https://www.npmjs.com/package/mongoose' target='blank'>
-                        Mongoose <i className='far fa-external-link'></i>
+                        Mongoose <i className='far fa-external-link' />
                     </a>{' '}
                     framework. The frontend is built with the{' '}
                     <a href='https://reactjs.org/' target='blank'>
-                        React <i className='far fa-external-link'></i>
+                        React <i className='far fa-external-link' />
                     </a>{' '}
                     library, utilizing Hooks and{' '}
                     <a href='https://www.styled-components.com/' target='blank'>
-                        Styled-Components <i className='far fa-external-link'></i>
+                        Styled-Components <i className='far fa-external-link' />
                     </a>{' '}
                     for its functionality and styling. Mongo Headlines is hosted on{' '}
                     <a href='https://www.heroku.com' target='blank'>
-                        Heroku <i className='far fa-external-link'></i>
+                        Heroku <i className='far fa-external-link' />
                     </a>{' '}
                     and the code is available for review on{' '}
                     <a href='https://github.com/mmcxii/mongo-headlines' target='blank'>
-                        Github <i className='far fa-external-link'></i>
+                        Github <i className='far fa-external-link' />
+                    </a>
+                    . The data is pulled from the{' '}
+                    <a href='https://developer.nytimes.com/' target='blank'>
+                        New York Times API <i className='far fa-external-link' />
                     </a>
                     .
                 </p>
                 <p>
                     Additionally, both the front and back ends are built using{' '}
                     <a href='https://www.typescriptlang.org/' target='blank'>
-                        TypeScript <i className='far fa-external-link'></i>
+                        TypeScript <i className='far fa-external-link' />
                     </a>
                     .
                 </p>
